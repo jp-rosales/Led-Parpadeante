a5c006e1d941d1c723ba43d2cdfc40c41c7b96dd
jp-rosales <rosalesr.juanpablo@gmail.com>
2025-08-21 20:11:59 -0600

    Add files via upload

---
+++ led_parpadeante.ino
@@ -0,0 +1,17 @@
+int led = 13;
+
+void setup() {
+  pinMode(led, OUTPUT);
+
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite(led, HIGH);
+  delay(200);
+  digitalWrite(led, LOW);
+  delay(200);
+  // put your main code here, to run repeatedly:
+
+}
